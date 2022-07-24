554fc2fc9fd7ca39f603d3ab6788f626c4eea73c
Salah Kaf <68357629+KAFSALAH@users.noreply.github.com>
2022-07-24 13:29:22 +0300

    Delete inserting, extending, appending.py

--- inserting, extending, appending.py
+++
@@ -1,8 +0,0 @@
-fruits = ["banana", "lemon"]
-newfruit = ["watermelon"]
-#Extending, add list to a list! pn, tuple can be used also! 
-fruits.extend(newfruit)
-print(fruits)
-# Append used to add to the end of the list 
-fruits.append("apple")
-print(fruits)
